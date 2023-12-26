e824cfd4b0aa25f044dbe261e606f36e17e2409b
masis-0 <iso.masatoshi@gmail.com>
2023-12-27 01:05:29 +0900

    add keybind insermode cursol move and add plugins.

--- neovim/.config/nvim/lua/plugins/code.lua
+++ neovim/.config/nvim/lua/plugins/code.lua
@@ -1,6 +1,40 @@
 return {
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  },
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	},
+	{
+		"telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+		{
+			"neovim/nvim-lspconfig",
+			opts = {
+				servers = {
+					solargraph = {},
+				},
+			},
+		},
+		{
+			"nvim-treesitter/nvim-treesitter",
+			opts = {
+				ensure_installed = {
+					"bash",
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"yaml",
+					"ruby",
+				},
+			},
+		},
+	},
 }
