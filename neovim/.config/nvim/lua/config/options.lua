fbd99557eccb2ce43455ebcf3813a51b67ed0acc
masis-0 <iso.masatoshi@gmail.com>
2023-12-29 23:18:31 +0900

    add default keymaps

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,1 +1,21 @@
+vim.g.mapleader = " "
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+
+vim.opt.autoindent = true
+vim.opt.backup = false
+vim.opt.ignorecase = true
+vim.opt.laststatus = 3
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.smarttab = true
 vim.opt.swapfile = false
+vim.opt.tabstop = 2
+vim.opt.title = true
+vim.opt.mouse = ""
+vim.opt.splitbelow = true
+vim.opt.splitright = true
