68498a3a605029e104c9a15738013211f23e6808
masis-0 <iso.masatoshi@gmail.com>
2024-01-01 11:59:44 +0900

    add editor.lua(old code.lua), also edit code.lua. Mainly, include coding plugin.

--- neovim/.config/nvim/lua/plugins/disale.lua
+++ neovim/.config/nvim/lua/plugins/disale.lua
@@ -3,4 +3,10 @@ return {
 		"nvim-neo-tree/neo-tree.nvim",
 		enabled = false,
 	},
+	{
+		"folke/flash.nvim",
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, false },
+		},
+	},
 }
---
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,107 @@
+return {
+	"telescope.nvim",
+	dependencies = {
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+		"nvim-telescope/telescope-file-browser.nvim",
+	},
+	keys = function()
+		return {
+			{
+				"<leader>fp",
+				function()
+					require("telescope.builtin").find_files({
+						cwd = require("lazy.core.config").options.root,
+					})
+				end,
+				desc = "Find Plugin File",
+			},
+			{
+				"<leader>ff",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.find_files({
+						no_ignore = false,
+						hidden = true,
+					})
+				end,
+				desc = "Find Files(cwd)",
+			},
+			{
+				"<leader>fg",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.live_grep()
+				end,
+				desc = "Live Grep(cwd)",
+			},
+			{
+				"<leader>fb",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.buffers()
+				end,
+				desc = "File Buffers(cwd)",
+			},
+			{
+				"<leader>fh",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.help_tags()
+				end,
+				desc = "Help Tags",
+			},
+			{
+				";;",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.resume()
+				end,
+				desc = "Resume",
+			},
+			{
+				"<leader>fd",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.diagnostics()
+				end,
+				desc = "Lists diagnostics",
+			},
+			{
+				"<leader>fs",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.treesitter()
+				end,
+				desc = "Lists Function names from TreeSitter",
+			},
+			{
+				"<leader>e",
+				function()
+					local telescope = require("telescope")
+
+					local function telescope_buffer_dir()
+						return vim.fn.expand("%:p:h")
+					end
+
+					telescope.extensions.file_browser.file_browser({
+						path = "%:p:h",
+						cwd = telescope_buffer_dir(),
+						respect_gitignore = false,
+						hidden = true,
+						grouped = true,
+						previewer = false,
+						initial_mode = "normal",
+						layout_config = { height = 40 },
+					})
+				end,
+				desc = "File Browser(cwd)",
+			},
+		}
+	end,
+}
