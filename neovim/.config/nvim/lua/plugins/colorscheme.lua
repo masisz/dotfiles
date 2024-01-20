110b9f86f5a38a6beffd33592c84f31504c738a5
masisz <iso.masatoshi@gmail.com>
2024-01-20 23:19:36 +0900

    remove comment

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,23 +4,4 @@ return {
 		lazy = true,
 		opts = { transparent = true },
 	},
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
-	},
-	-- {
-	-- "nvim-lualine/lualine.nvim",
-	-- event = "VeryLazy",
-	-- opts = {
-	-- 	options = {
-	-- 		theme = "catppuccin",
-	-- 	},
-	-- },
-	-- },
 }
